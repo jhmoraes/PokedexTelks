3d24eb0b967e648e7004b9ac9e4a6ee0b6e42a6b
bruhoffman <bruhoffman@gmail.com>
2024-05-28 14:59:53 -0300

    Criação das Rotas

---
+++ src/Routes/Router.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import PokemonDetailPage from "../Pages/PokemonDetailPage/PokemonDetailPage";
+import PokemonListPage from "../Pages/PokemonsListPage/PokemonsListPage";
+import Error from "../Pages/Error/Error";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemondetail" element={<PokemonDetailPage />} />
+        <Route path="/pokemonlist" element={<PokemonListPage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
