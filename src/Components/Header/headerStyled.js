4c5e241ab7ce5fd2a18e652808a7bd9fbd8e9a66
roliberdade14 <roliberdade1433@gmail.com>
2024-05-28 22:11:59 -0300

    header-problemas-css

--- src/Components/Header/headerStyled.js
+++ src/Components/Header/headerStyled.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 
-export const Container = styled.header`
+export const ContainerHeader = styled.div`
     display: flex;
     gap: 16px;
     padding: 16px;
-    border: 1px solid #000;`
+    border: 1px solid #000;
+`
+export const Container = styled.header`
+background-color: blue;
+`
