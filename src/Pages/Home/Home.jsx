0d40fc5cf872b1bf1ba79d0c6bc9d7901bd65eb9
bruhoffman <bruhoffman@gmail.com>
2024-06-03 14:03:42 -0300

    Alterações

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,13 @@
 import Header from "../../Components/Header/Header"
 import { CardsContainer } from "./homeStyled"
-import pokemons from "../../pokemon/pokemon.json"
 import PokemonCard from "../../Components/PokemonCard/PokemonCard"
 
 const Home = () => {
     return (
         <>
             <Header />
-
-            <CardsContainer>
-                {pokemons.map((pokemon) =>{
-                    return 
-                    <PokemonCard cardColor={getColors(pokemon.type[0])} key={pokemon.id} pokemon={pokemon} />
-                })}                
-            </CardsContainer>
+            <h1>Home</h1>
+            
 
         </>
     )
