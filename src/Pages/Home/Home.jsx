e29b06c3729d60ae4287196c7b0a7afda42543a2
julianaMoraes <juliana.hmoraes@gmail.com>
2024-06-18 23:08:28 -0300

    commit do deploy

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import GlobalStateContext from "../../global/GlobalStateContext";
 import PokemonCard from "../../Components/PokemonCard/PokemonCard"
 
 const Home = () => {
-
+    //teste
     const { allPokemons, pokedex, addToPokedex } = useContext(GlobalStateContext);
 
     const filteredPokeList = () =>
